949ea74ae85f605b5e1d68231fed85afdf82d8c0
angelo-perez <92904983+angelo-perez@users.noreply.github.com>
2024-04-01 10:57:12 +0800

    Added "ValidateNever" attribute to "Phone" property in Specification Model

--- PasaPhone.Models/Specification.cs
+++ PasaPhone.Models/Specification.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -13,6 +14,7 @@ namespace PasaPhone.Models
     {
         public int SpecificationId { get; set; }
         [ForeignKey("SpecificationId")]
+        [ValidateNever]
         public Phone? Phone { get; set; }
 
         public string? Chipset { get; set; }
